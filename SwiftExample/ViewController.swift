a3d6db940fd99cb3f30b5d55dbebb50c09e44353
Arjun Patel <arjun.patel@jini.guru>
2019-09-05 14:28:02 +0530

    Init project

--- SwiftExample/ViewController.swift
+++ SwiftExample/ViewController.swift
@@ -7,19 +7,124 @@
 //
 
 import UIKit
+import SQLite3
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
 
+    var db: OpaquePointer?
+    var heroList = [Hero]()
+    
+    @IBOutlet weak var tableViewHeroes: UITableView!
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldPowerRanking: UITextField!
+    
+    @IBAction func buttonSave(_ sender: UIButton) {
+        let name = textFieldName.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let powerRanking = textFieldPowerRanking.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if(name?.isEmpty)!{
+            textFieldName.layer.borderColor = UIColor.red.cgColor
+            return
+        }
+        
+        if(powerRanking?.isEmpty)!{
+            textFieldName.layer.borderColor = UIColor.red.cgColor
+            return
+        }
+        
+        var stmt: OpaquePointer?
+        
+        let queryString = "INSERT INTO Heroes (name, powerrank) VALUES (?,?)"
+        
+        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error preparing insert: \(errmsg)")
+            return
+        }
+        
+        if sqlite3_bind_text(stmt, 1, name, -1, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure binding name: \(errmsg)")
+            return
+        }
+        
+        if sqlite3_bind_int(stmt, 2, (powerRanking! as NSString).intValue) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure binding name: \(errmsg)")
+            return
+        }
+
+        if sqlite3_step(stmt) != SQLITE_DONE {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure inserting hero: \(errmsg)")
+            return
+        }
+        
+        textFieldName.text=""
+        textFieldPowerRanking.text=""
+        
+        readValues()
+
+        print("Herro saved successfully")
+    }
+    
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return heroList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        let hero: Hero
+        hero = heroList[indexPath.row]
+        cell.textLabel?.text = hero.name
+        return cell
+    }
+    
+    
+    func readValues(){
+        heroList.removeAll()
+
+        let queryString = "SELECT * FROM Heroes"
+        
+        var stmt:OpaquePointer?
+        
+        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error preparing insert: \(errmsg)")
+            return
+        }
+        
+        while(sqlite3_step(stmt) == SQLITE_ROW){
+            let id = sqlite3_column_int(stmt, 0)
+            let name = String(cString: sqlite3_column_text(stmt, 1))
+            let powerrank = sqlite3_column_int(stmt, 2)
+            
+            heroList.append(Hero(id: Int(id), name: String(describing: name), powerRanking: Int(powerrank)))
+        }
+        
+        self.tableViewHeroes.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("HeroesDatabase.sqlite")
+        
+        
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("error opening database")
+        }
+        
+        if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Heroes (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, powerrank INTEGER)", nil, nil, nil) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error creating table: \(errmsg)")
+        }
+        
+        readValues()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
